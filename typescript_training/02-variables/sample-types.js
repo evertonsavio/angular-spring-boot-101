480fd2567e246ecfec3abc854a73feb81c026dba
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-18 17:22:26 -0300

    :tada: TypeScript, so.. it begings

---
+++ typescript_training/02-variables/sample-types.js
@@ -0,0 +1,11 @@
+var found = true;
+var grade = 88.6;
+var firstName = 'Everton';
+var lastName = 'Savio';
+console.log(found);
+console.log('The grade is ' + grade);
+console.log('Hi ' + firstName + ' ' + lastName);
+//found = 'teste';
+/* 10 found = 'teste';
+   ~~~~~
+Found 1 error. */
---
+++ typescript_training/02-variables/sample-types.ts
@@ -0,0 +1,14 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = 'Everton';
+let lastName: string = 'Savio';
+
+console.log(found);
+console.log('The grade is ' + grade);
+console.log('Hi ' + firstName + ' ' + lastName);
+
+//found = 'teste';
+
+/* 10 found = 'teste';
+   ~~~~~
+Found 1 error. */
