7111a8af0faafcc3b11dfa230f8fab0683e3f419
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-18 19:04:20 -0300

    :boom: Inheritance TypeScript

--- typescript_training/05-inheritance/Driver.js
+++ typescript_training/05-inheritance/Driver.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Shape_1 = require("./Shape");
 var Circle_1 = require("./Circle");
+var Retangle_1 = require("./Retangle");
 var myShape = new Shape_1.Shape(10, 15);
 console.log(myShape.getInfo());
 var myCircle = new Circle_1.Circle(5, 10, 15);
 console.log(myCircle.getInfo());
+var myRetangle = new Retangle_1.Retangle(0, 0, 3, 7);
+console.log(myRetangle.getInfo());
