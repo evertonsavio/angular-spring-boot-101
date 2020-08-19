d15c47de7b6df5d78fbc43b5be2f6ba2f01e55e1
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-18 22:47:21 -0300

    :sparkles: JPA Annotations na Entity

--- spring_boot_ecommerce/spring-boot-ecommerce/src/main/java/dev/evertonsavio/ecommerce/entity/Product.java
+++ spring_boot_ecommerce/spring-boot-ecommerce/src/main/java/dev/evertonsavio/ecommerce/entity/Product.java
@@ -1,9 +1,9 @@
 package dev.evertonsavio.ecommerce.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -11,25 +11,38 @@ import java.util.Date;
 @Table(name="Produto")
 @Data //Lombok !!! WoW!!!
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
+    @Column(name="sku")
     private String sku;
 
+    @Column(name="name")
     private String name;
 
+    @Column(name="description")
     private String description;
 
+    @Column(name="unit_price")
     private BigDecimal unitPrice;
 
+    @Column(name="image_url")
     private String imageUrl;
 
+    @Column(name="active")
     private boolean active;
 
+    @Column(name="units_in_stock")
     private int  unitsInStock;
 
-    private Date dataCreated;
+    @Column(name="date_created")
+    @CreationTimestamp
+    private Date dateCreated;
 
+    @CreationTimestamp
+    @Column(name="last_updated")
     private Date lastUpdated;
 
 }
